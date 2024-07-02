30440e71930ccee97e69894340f044ffa88b80f3
Sajid <sajidkp028@gmail.com>
2024-07-02 17:04:22 +0500

    secret auction program done

---
+++ main.py
@@ -0,0 +1,23 @@
+bids = {}
+bidding_finished = False
+
+def find_highest_bidding(total_bids):
+    highest_bids = 0
+    winner = ""
+    for bids in total_bids:
+        bids_amount = total_bids[bids]
+        if bids_amount > highest_bids:
+            highest_bids = bids_amount
+            winner = bids
+        print(f"The winner is {bids} with bids of ${highest_bids}")
+
+while not bidding_finished:
+    name = input("What is your name? ")
+    bid_amount = int(input("What is your bid? $"))
+    bids[name] = bid_amount
+    Should_continue = input("Is there any other user who want to bid? type yes or no: ")
+    if Should_continue == "no":
+        bidding_finished = True
+        find_highest_bidding(bids)
+
+
